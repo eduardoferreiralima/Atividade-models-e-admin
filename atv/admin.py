e5ecf0d4e61a8079b9dfaaee45cd50341728285c
ferreira.lima1 <ferreira.lima1@escolar.ifrn.edu.br>
2024-12-12 20:00:43 -0300

    criação e execuçao das migrações

--- atv/admin.py
+++ atv/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Products
-# Register your models here.
-admin.site.register(Products)
+
+from django.contrib import admin
+from .models import Products, Categoria, Fornecedor
+
+
+
+@admin.register(Products)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nome', 'preco', 'quantidade_estoque', 'data_criacao')  # Campos na listagem
+    search_fields = ('codigo', 'nome')  # Campos para busca
+    list_filter = ('data_criacao',)  # Filtro por data de criação
+    ordering = ('-data_criacao',)  # Ordenação decrescente
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'contato')
+    search_fields = ('nome',)
--- atv/models.py
+++ atv/models.py
@@ -2,19 +2,20 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=25)
+
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=35)
+    endereco = models.CharField(max_length=50)
+    contato = models.CharField(max_length=20)
 
 class Products(models.Model):
-    nome_produto = models.CharField(null=False, max_length=65)
-    codigo_produto = models.CharField(primary_key=True, null=False, max_length=12)
+    nome = models.CharField(null=False, max_length=65)
+    codigo = models.CharField(primary_key=True, null=False, max_length=12)
     descricao = models.CharField(null=True, max_length=500)
     preco = models.DecimalField(decimal_places=2, max_digits=5)
     quantidade_estoque = models.IntegerField()
     data_criacao = models.DateTimeField(auto_created=True)
-
-
-'''Nome do produto (String, obrigatório).
-Código do produto (String, único, obrigatório).
-Descrição (Texto longo, opcional).
-Preço (Decimal com 2 casas decimais).
-Quantidade em estoque (Número inteiro).
-Data de criação (Registrada automaticamente).'''
+    categorias = models.ManyToManyField(Categoria, related_name='produtos')
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE, related_name='produtos')
